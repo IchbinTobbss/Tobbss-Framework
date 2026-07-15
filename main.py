7d6e60d0efd91ba81b8e133558daf6075642ecc1
Tobias Leingartner <tleingartner@icloud.com>
2026-07-15 22:17:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,50 @@
 import subprocess
 import sys
 import importlib
+
+
 print("Initializing...")
 print("Done.")
-print("Checking for dependencies...")
-
 
 packages = {
     "arcade": "arcade",
-    "pymunk" : "pymunk",
-    "ollama" : "ollama",
-    "speech_recognition" : "SpeechRecognition",
-    "pyttsx3" : "pyttsx3",
-    "pyaudio" : "PyAudio",
+    "pymunk": "pymunk",
+    "ollama": "ollama",
+    "speech_recognition": "SpeechRecognition",
+    "pyttsx3": "pyttsx3",
+    "pyaudio": "PyAudio",
 }
 
-for module, package in packages.items():
-    try:
-        importlib.import_module(module)
-        print(f"{package} is installed")
-    except ImportError:
-        print(f"Installiere {package}...")
-        subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
-print("Dependencies are installed")
+def install_dependencies():
+    print("Checking for dependencies...")
+
+    for module, package in packages.items():
+        try:
+            importlib.import_module(module)
+            print(f"{package} is installed")
+
+        except ImportError:
+            print(f"Installing {package}...")
+
+            subprocess.check_call([
+                sys.executable,
+                "-m",
+                "pip",
+                "install",
+                package
+            ])
+
+    print("Dependencies are installed")
+
+
+def main():
+    if "--install" in sys.argv:
+        install_dependencies()
+        return
+
+    print("Tobi Framework started")
+
+
+if __name__ == "__main__":
+    main()
